d77614ce5e1db8c5f45a30118ed62158f9fb3ca6
anilkumarsheoran <anilkumar.sheoran@gmail.com>
2016-06-16 23:56:16 +0530

    Aboutme added

--- static/js/app.js
+++ static/js/app.js
@@ -6,14 +6,21 @@ app.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
     
     $stateProvider
         .state('home', {
-          url: '/',
-          views: {
-            'home': { templateUrl: 'index.html',controller:'MyController'},
-            'header@home': { templateUrl: 'partials/header.html' },
-            'body@home': { templateUrl: 'partials/body.html'},
-            'footer@home': { templateUrl: 'partials/footer.html' }
-         }
-      });
+            url: '/',
+            views: {
+                'home': { templateUrl: 'index.html', controller: 'MyController' },
+                'header@home': { templateUrl: 'partials/header.html' },
+                'body@home': { templateUrl: 'partials/body.html' },
+                'footer@home': { templateUrl: 'partials/footer.html' }
+            }
+        })
+    .state('home.Aboutme', {
+        parent: 'home',
+        url: 'About Me',
+        views: {
+            'body@home': { templateUrl: 'static/html/Contactus.html' },    
+      }
+    });
         
    $locationProvider.html5Mode(true);
   
