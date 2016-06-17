4559ddd1909dd0fe9ef3d4f74b80b5e3b8589e57
anilkumarsheoran <anilkumar.sheoran@gmail.com>
2016-06-17 15:53:58 +0530

    Structure

--- static/js/app.js
+++ static/js/app.js
@@ -32,15 +32,65 @@ app.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
                   templateUrl: 'templates/assets/nav.html'
                 },
                 'body@aboutme': {
-                  templateUrl: 'templates/body.html'
+                    templateUrl: 'templates/body.html'
                 },
                 'footer@aboutme': {
                   templateUrl: 'templates/assets/footer.html'
                 }
               }
         })
+     .state('resume', {
+         url: '/resume',
+              views: {
+                '': {
+                  templateUrl: 'templates/main.html'
+                },
+                'nav@resume': {
+                  templateUrl: 'templates/assets/nav.html'
+                },
+                'body@resume': {
+                    templateUrl: 'static/html/resume.html'
+                },
+                'footer@resume': {
+                  templateUrl: 'templates/assets/footer.html'
+                }
+              }
+        })
     
-
+      .state('contact', {
+          url: '/contact',
+          views: {
+              '': {
+                  templateUrl: 'templates/main.html'
+              },
+              'nav@contact': {
+                  templateUrl: 'templates/assets/nav.html'
+              },
+              'body@contact': {
+                  templateUrl: 'static/html/contact.html'
+              },
+              'footer@contact': {
+                  templateUrl: 'templates/assets/footer.html'
+              }
+          }
+      })
+      .state('resources', {
+          url: '/resources',
+          views: {
+              '': {
+                  templateUrl: 'templates/main.html'
+              },
+              'nav@resources': {
+                  templateUrl: 'templates/assets/nav.html'
+              },
+              'body@resources': {
+                  templateUrl: 'static/html/resource.html'
+              },
+              'footer@resources': {
+                  templateUrl: 'templates/assets/footer.html'
+              }
+          }
+      })
         
    $locationProvider.html5Mode(true);
   
