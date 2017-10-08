93dae747353404dabe0e7cf82cada469f2a15dd9
anilkumarsheoran <anilkumarsheoran@gmail.com>
2017-10-09 01:39:34 +0530

    API integration

--- static/js/controller.js
+++ static/js/controller.js
@@ -22,7 +22,47 @@ appControllers.controller('contactController',function($scope,$http,$stateParams
      $scope.contact = 'contacts';
     $scope.submitclicked=function () {
         alert("Button is Enabled");
+       //  var url = "http://localhost:3001/api/userdata";
+       //  var userdata ={
+       //      'firstName': $scope.firstName,
+       //      'lastName': $scope.lastName,
+       //      'emailid': $scope.email,  
+       //      'created_date':'21'    
+       //  };
+       // $http.post(url,userdata).success( function(data,status,header,config) {
+       //    $scope.msg = data; 
+       // }).error(function(data, status, headers, config) {
+       //      alert( "failure message: " + JSON.stringify({data: data}));
+       //  });
 
+       $http({
+            url: "http://localhost:3001/api/userdata",
+            method: "POST",
+            data: {
+            'firstName': $scope.firstName,
+            'lastName': $scope.lastName,
+            'emailid': $scope.email,  
+            'created_date':'21'    
+            },
+            contentType:'application/json'//,
+           // headers: {
+           // 'Access-Control-Allow-Origin' : '*',
+           // }
+        })
+        .then(function(response) {
+                // success
+        }, 
+        function(response) { // optional
+                // failed
+        });
+       $scope.firstName='',
+       $scope.lastName='',
+       $scope.email=''
+    };
+    $scope.reterivedata=function(){
+         $http.get('http://localhost:3001/api/userdata').success( function(response) {
+            $scope.reterived = response; 
+        });
     }
 });
 
